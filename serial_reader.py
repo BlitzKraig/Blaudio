f4724f80e48a8872474db3236f82a115a62cf9d5
BlitzKraig <craig@blitzkraig.com>
2024-06-02 15:51:45 +0100

    Add automatic connection retrying

--- serial_reader.py
+++ serial_reader.py
@@ -3,24 +3,48 @@ import threading
 import time
 from collections import deque
 import numpy as np
+from PyQt6.QtCore import QTimer
 
 class SerialReader:
-    def __init__(self, port, callback, baudrate=9600, callback_interval=0.05, smoothing_window=10, old_range=(0, 1023), new_range=(0, 100)):
+    def __init__(self, port, callback, baudrate=9600, callback_interval=0.05, smoothing_window=10, old_range=(0, 1023), new_range=(0, 100), retry_interval=5):
         self.old_min, self.old_max = old_range
         self.new_min, self.new_max = new_range
-        self.ser = serial.Serial(port, baudrate)
+        self.port = port
+        self.baudrate = baudrate
+        self.retry_interval = retry_interval
         self.callback = callback
         self.callback_interval = callback_interval
         self.knobs = {}
         self.knob_buffers = {}
         self.smoothing_window = smoothing_window
         self.last_callback_time = time.time()
-        self.thread = threading.Thread(target=self.read_from_port)
-        self.thread.daemon = True
-        self.thread.start()
+        
+        self.is_connected = False
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.try_connect)
+        self.try_connect()
+        self.timer.start(retry_interval * 1000)
+        
 
+    def try_connect(self):
+        if self.is_connected:
+            return
+        print("Trying to connect to the serial port...")
+        try:
+            self.ser = serial.Serial(self.port, self.baudrate)
+            self.thread = threading.Thread(target=self.read_from_port)
+            self.thread.daemon = True
+            self.thread.start()
+            # self.timer.stop()  # Stop the timer once connected ~ Not stopping, as we're using this to keep us connected now
+            print("Connected to the serial port")
+            self.is_connected = True
+        except serial.SerialException:
+            print("Failed to connect to the serial port. Retrying in {} seconds...".format(self.retry_interval))
+            self.is_connected = False
+            
     def read_from_port(self):
+        
         while True:
             try:
                 if self.ser.in_waiting > 0:
@@ -40,4 +64,5 @@ class SerialReader:
                         self.last_callback_time = current_time
             except serial.SerialException:
                 print("SerialException occurred. Stopping reader.")
+                self.is_connected = False
                 break
