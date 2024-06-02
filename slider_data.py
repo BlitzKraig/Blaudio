d2cfa975a29c19fb72a7ad01fcaf0a1fd318b045
BlitzKraig <craig@blitzkraig.com>
2024-06-02 16:55:36 +0100

    Fix datafile for standalone builds

--- slider_data.py
+++ slider_data.py
@@ -1,13 +1,22 @@
+import os
+import sys
 import pickle
 from slider import Slider
 
 class SliderData:
     def __init__(self, parent):
         self.parent = parent
+        if getattr(sys, 'frozen', False):
+            # The application is running as a standalone executable
+            application_path = os.path.dirname(sys.executable)
+        else:
+            # The application is running from a script (e.g., via VS Code)
+            application_path = os.path.dirname(os.path.abspath(__file__))
+        self.data_file = os.path.join(application_path, 'slider_data.pkl')
         
     def load(self):
         try:
-            with open('slider_data.pkl', 'rb') as f:
+            with open(self.data_file, 'rb') as f:
                 slider_data = pickle.load(f)
         except FileNotFoundError:
             return
@@ -18,7 +27,7 @@ class SliderData:
 
     def save(self, should_notify=True):
         slider_data = [slider.serialize() for slider in self.parent.sliders]
-        with open('slider_data.pkl', 'wb') as f:
+        with open(self.data_file, 'wb') as f:
             pickle.dump(slider_data, f)
         if should_notify:
             self.parent.show_notification('Slider data saved successfully!')
